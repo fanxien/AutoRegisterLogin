9e8b2bcdbf7e3d0cdcde6debb9fc34914902f237
fish_a <3029136568@qq.com>
2026-06-14 23:42:33 +0800

    refactor: rewrite plugin as thin shell, wire IP whitelist modules

--- AutoRegisterLogin/AutoRegisterLoginPlugin.cs
+++ AutoRegisterLogin/AutoRegisterLoginPlugin.cs
@@ -1,7 +1,9 @@
 using System.Reflection;
+using System.IO;
 using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
+using TShockAPI.Hooks;
 
 namespace AutoRegisterLogin;
 
@@ -9,6 +11,9 @@ namespace AutoRegisterLogin;
 public sealed class AutoRegisterLoginPlugin : TerrariaPlugin
 {
     private PluginConfig _config = new();
+    private IpWhitelist _whitelist = null!;
+    private PendingApprovalStore _pendingStore = null!;
+    private AuthenticationService _authService = null!;
 
     public AutoRegisterLoginPlugin(Main game)
         : base(game)
@@ -28,12 +33,82 @@ public sealed class AutoRegisterLoginPlugin : TerrariaPlugin
 
     public override void Initialize()
     {
-        _config = PluginConfig.Load();
+        LoadConfig();
+
+        var whitelistPath = Path.Combine(TShock.SavePath, "AutoRegisterLogin_whitelist.json");
+        _whitelist = new IpWhitelist(whitelistPath);
+        _whitelist.Load();
+
+        var pendingPath = Path.Combine(TShock.SavePath, "AutoRegisterLogin_pending.json");
+        _pendingStore = new PendingApprovalStore(
+            pendingPath, _config.PendingApprovalTimeoutHours);
+        _pendingStore.Load();
+
+        _authService = new AuthenticationService(_config, _whitelist, _pendingStore);
+
+        ServerApi.Hooks.ServerJoin.Register(this, OnJoin);
+        ServerApi.Hooks.NetGreetPlayer.Register(this, OnGreetPlayer);
+        GeneralHooks.ReloadEvent += OnReload;
+
+        AdminCommands.Register(_whitelist, _pendingStore);
+
         TShock.Log.ConsoleInfo("[AutoRegisterLogin] Plugin initialized.");
     }
 
     protected override void Dispose(bool disposing)
     {
+        if (disposing)
+        {
+            ServerApi.Hooks.ServerJoin.Deregister(this, OnJoin);
+            ServerApi.Hooks.NetGreetPlayer.Deregister(this, OnGreetPlayer);
+            GeneralHooks.ReloadEvent -= OnReload;
+        }
+
         base.Dispose(disposing);
+    }
+
+    private void OnReload(ReloadEventArgs args)
+    {
+        LoadConfig();
+
+        var pendingPath = Path.Combine(TShock.SavePath, "AutoRegisterLogin_pending.json");
+        _pendingStore = new PendingApprovalStore(
+            pendingPath, _config.PendingApprovalTimeoutHours);
+        _pendingStore.Load();
+
+        _authService = new AuthenticationService(_config, _whitelist, _pendingStore);
+
+        args.Player?.SendSuccessMessage("[AutoRegisterLogin] Configuration reloaded.");
+    }
+
+    private void LoadConfig()
+    {
+        _config = PluginConfig.Load();
+    }
+
+    private void OnJoin(JoinEventArgs args)
+    {
+        TryAuthenticate(args.Who, "join");
+    }
+
+    private void OnGreetPlayer(GreetPlayerEventArgs args)
+    {
+        TryAuthenticate(args.Who, "greet");
+    }
+
+    private void TryAuthenticate(int who, string stage)
+    {
+        if (who < 0 || who >= TShock.Players.Length)
+        {
+            return;
+        }
+
+        var player = TShock.Players[who];
+        if (player == null)
+        {
+            return;
+        }
+
+        _authService.TryAuthenticate(player, stage);
     }
 }
